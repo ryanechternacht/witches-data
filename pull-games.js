d6e786baa86c3c0456894808132a5feb9289ce23
ryanechternacht <ryan@echternacht.org>
2016-01-21 16:03:10 -0500

    Changed the approach to be more synchronous, since this is more inline with what I'm trying to do.

--- pull-games.js
+++ pull-games.js
@@ -3,9 +3,11 @@ var DocumentClient = require('documentdb').DocumentClient,
     argv = require('minimist')(process.argv.slice(2)),
     _ = require('underscore'),
     moment = require('moment'),
-    http = require('http'),
-    Promise = require('promise'),
-    fs = require('fs');
+    // http = require('http'),
+    // Promise = require('promise'),
+    httpSync = require('http-sync'),
+    fs = require('fs'),
+    sleep = require('sleep');
 
 if(argv['?'] || argv.h || _.contains(argv._, 'help')) { 
     console.log('\t-a \t\t start at first game on snellman');
@@ -19,33 +21,33 @@ if(argv['?'] || argv.h || _.contains(argv._, 'help')) {
 var defaults = setupDefaults(),
     tempFile = setupTempFile(argv, defaults),
     date = setupDate(argv, defaults),
+    today = moment(),
     host = azureInfo.host,
     masterKey = azureInfo.masterKey;
 
-lookupDate(date).then(function(results) { 
-    var gameList = _.keys(results.games);
-    console.log(gameList);
-    writeStatus(tempFile, date, gameList);
-
-    setInterval(function() { 
-        console.log(gameList.length);
-        var game = gameList.shift();
-        console.log(game);
-
-        pullGame(game).then(function(data) { 
-            console.log(data[100].commands);
-        });
-    }, 30000); //30ms
-});
-
-// function getGames(gameList) { 
-//     var game = gameList.shift();
-//     console.log(game);
-
-//     pullGame(game).then(function(data) { 
-//         console.log(data[100].commands);
-//     });
+// while(date < today) { 
+    
 // }
+
+var data = lookupDateSync(date);
+var gameList = _.keys(data.games);
+writeStatus(tempFile, date, gameList);
+while(gameList.length > 0) { 
+    console.log(gameList.length);
+    var game = gameList.shift();
+    console.log(game);
+    var ledger = pullGameSync(game);
+    console.log(ledger[100]);
+    console.log();
+    sleep.sleep(10); // 10s
+}
+
+// var today = moment();
+// for(/*date*/; date <= today; date = date.add(1, 'day')) { 
+//     console.log('http://terra.snellman.net/app/results/v2/' + date.format('YYYY/MM/DD'));
+// }
+
+
 
 
 function setupDefaults() {
@@ -82,7 +84,41 @@ function setupDate(argv, defaults) {
 }
 
 // returns {player, games}
-function lookupDate(date) { 
+// function lookupDate(date) { 
+//     var path = '/app/results/v2/' + date.format('YYYY/MM/DD');
+//     var options = {
+//         host: 'terra.snellman.net',
+//         port: 80,
+//         path: path,
+//         method: 'GET'
+//     };
+
+//     return new Promise(function (resolve, reject) {
+//         var request = http.request(options);
+//         var data;
+
+//         request.on('response', function(response) {
+//             var tmp = '';
+
+//             response.on('data', function(chunk) {
+//                 // console.log('chunk');
+//                 tmp += chunk;
+//             });
+
+//             response.on('end', function() { 
+//                 // console.log('end');
+//                 data = JSON.parse(tmp);
+//                 resolve({
+//                     players: data.players,
+//                     games: data.games
+//                 });
+//             });
+//         }).end(); // invoke immediately
+//     });
+// }
+
+// returns {player, games}
+function lookupDateSync(date) { 
     var path = '/app/results/v2/' + date.format('YYYY/MM/DD');
     var options = {
         host: 'terra.snellman.net',
@@ -91,28 +127,21 @@ function lookupDate(date) {
         method: 'GET'
     };
 
-    return new Promise(function (resolve, reject) {
-        var request = http.request(options);
-        var data;
-
-        request.on('response', function(response) {
-            var tmp = '';
-
-            response.on('data', function(chunk) {
-                // console.log('chunk');
-                tmp += chunk;
-            });
-
-            response.on('end', function() { 
-                // console.log('end');
-                data = JSON.parse(tmp);
-                resolve({
-                    players: data.players,
-                    games: data.games
-                });
-            });
-        }).end(); // invoke immediately
+    var request = httpSync.request(options);
+    var timeout = false;
+    request.setTimeout(10000, function() {
+        timeout = true;
     });
+    var response = request.end(); // execute synchronously
+    
+    if(timeout) {
+        throw new Error("lookupDateSync timed out");
+    }
+    var data = JSON.parse(response.body.toString());
+    return {
+        players: data.players,
+        games: data.games
+    }
 }
 
 function writeStatus(tempFile, date, gameList) { 
@@ -127,7 +156,37 @@ function writeStatus(tempFile, date, gameList) {
     });
 }
 
-function pullGame(game) { 
+// function pullGame(game) { 
+//     var path = '/app/view-game/?game=' + game;
+//     var options = {
+//         host: 'terra.snellman.net',
+//         port: 80,
+//         path: path,
+//         method: 'GET'
+//     };
+
+//     return new Promise(function (resolve, reject) {
+//         var request = http.request(options);
+//         var data;
+
+//         request.on('response', function(response) {
+//             var tmp = '';
+
+//             response.on('data', function(chunk) {
+//                 // console.log('chunk');
+//                 tmp += chunk;
+//             });
+
+//             response.on('end', function() { 
+//                 // console.log('end');
+//                 data = JSON.parse(tmp);
+//                 resolve(data.ledger);
+//             });
+//         }).end(); // invoke immediately
+//     });
+// }
+
+function pullGameSync(game) { 
     var path = '/app/view-game/?game=' + game;
     var options = {
         host: 'terra.snellman.net',
@@ -136,31 +195,19 @@ function pullGame(game) {
         method: 'GET'
     };
 
-    return new Promise(function (resolve, reject) {
-        var request = http.request(options);
-        var data;
-
-        request.on('response', function(response) {
-            var tmp = '';
-
-            response.on('data', function(chunk) {
-                // console.log('chunk');
-                tmp += chunk;
-            });
-
-            response.on('end', function() { 
-                // console.log('end');
-                data = JSON.parse(tmp);
-                resolve(data.ledger);
-            });
-        }).end(); // invoke immediately
+    var request = httpSync.request(options);
+    var timeout = false;
+    request.setTimeout(10000, function() {
+        timeout = true;
     });
+    var response = request.end(); // execute synchronously
+    
+    if(timeout) {
+        throw new Error("pullGameSync timed out");
+    }
+    var data = JSON.parse(response.body.toString());
+    return data.ledger;
 }
-
-// var today = moment();
-// for(/*date*/; date <= today; date = date.add(1, 'day')) { 
-//     console.log('http://terra.snellman.net/app/results/v2/' + date.format('YYYY/MM/DD'));
-// }
 
 // var client = new DocumentClient(host, {masterKey: masterKey});
 
